98dbb0712f336cab592cd2b622ad1eec3cf61a7d
Brian Beeler <132368299+brian-beeler@users.noreply.github.com>
2023-05-30 19:36:16 -0400

    Update privoxy.sh

--- privoxy.sh
+++ privoxy.sh
@@ -385,7 +385,7 @@ elif [[ $1 == "filter" ]]; then
 
 # 
 else
-    echo "usage: ./privoxy.sh [start|stop|restart|status|config|filter|log]"
+  echo "usage: ./privoxy.sh [start|stop|restart|status|config|filter|log]"
   echo "  start                   start server"
   echo "  stop                    stop server"
   echo "  restart                 restart"
