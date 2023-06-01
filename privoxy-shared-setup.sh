3ab1368b4b0b2df95c5b256fac62cd461175db9f
Brian Beeler <132368299+brian-beeler@users.noreply.github.com>
2023-06-01 11:50:35 -0400

    cat for echo on setup.sh

--- privoxy-shared-setup.sh
+++ privoxy-shared-setup.sh
@@ -11,5 +11,5 @@ echo -e "\r\n\r\n# \r\n# \r\n# \r\n# " >> /usr/local/etc/privoxy/config
 echo -e "# allow privoxy to make connections with the local network" >> /usr/local/etc/privoxy/config
 echo -e "listen-address $(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')":8118 >> /usr/local/etc/privoxy/config
 echo -e "# \r\n# \r\n# \r\n# \r\n" >> /usr/local/etc/privoxy/config
-echo -e /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
+cat /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
 cp /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.bak
--- privoxy-solo-setup.sh
+++ privoxy-solo-setup.sh
@@ -8,5 +8,5 @@ if [ -f "/usr/local/etc/privoxy/config" ]; then
   mv /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.original
 fi
 echo -e "\r\n\r\n# \r\n# \r\n# \r\n# \r\n" >> /usr/local/etc/privoxy/config
-echo -e /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
+cat /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
 cp /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.bak
