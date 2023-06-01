3874b40b8465b0b1c26eae88b2f0365e481c777d
Brian Beeler <132368299+brian-beeler@users.noreply.github.com>
2023-06-01 12:01:30 -0400

    more setup.sh updates

--- privoxy-shared-setup.sh
+++ privoxy-shared-setup.sh
@@ -3,13 +3,13 @@
 # a script to setup Privoxy for Privoxy-Pilot which also allows local network clients to use Privoxy
 
 curl -o "/usr/local/etc/privoxy/ppilot.sh" "https://raw.githubusercontent.com/brian-beeler/privoxy-pilot-macos/main/ppilot.sh"
-chmod og+x /usr/local/etc/privoxy/ppilot.sh
+chmod a+x /usr/local/etc/privoxy/ppilot.sh
 if [ -f "/usr/local/etc/privoxy/config" ]; then
   mv /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.original
 fi
 echo -e "\r\n\r\n# \r\n# " >> /usr/local/etc/privoxy/config
 echo -e "# allow privoxy to make connections with the local network" >> /usr/local/etc/privoxy/config
 echo -e "listen-address $(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')":8118 >> /usr/local/etc/privoxy/config
-echo -e "# \r\n" >> /usr/local/etc/privoxy/config
+echo -e "# \r\n# \r\n" >> /usr/local/etc/privoxy/config
 cat /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
 cp /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.bak
--- privoxy-solo-setup.sh
+++ privoxy-solo-setup.sh
@@ -3,10 +3,10 @@
 #a script to setup Privoxy for Privoxy-Pilot
 
 curl -o "/usr/local/etc/privoxy/ppilot.sh" "https://raw.githubusercontent.com/brian-beeler/privoxy-pilot-macos/main/ppilot.sh"
-chmod og+x /usr/local/etc/privoxy/ppilot.sh
+chmod a+x /usr/local/etc/privoxy/ppilot.sh
 if [ -f "/usr/local/etc/privoxy/config" ]; then
   mv /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.original
 fi
-echo -e "\r\n\r\n# \r\n# " >> /usr/local/etc/privoxy/config
+echo -e "\r\n \r\n # \r\n# \r\n" >> /usr/local/etc/privoxy/config
 cat /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
 cp /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.bak
