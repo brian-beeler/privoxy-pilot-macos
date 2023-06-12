91a2afbc829b5cece3130e2b0ef3d4f54a16264e
Brian Beeler <132368299+brian-beeler@users.noreply.github.com>
2023-06-12 08:18:05 -0400

    Update privoxy-solo-reset.sh

--- privoxy-solo-reset.sh
+++ privoxy-solo-reset.sh
@@ -2,6 +2,10 @@
 #!/bin/bash
 # a script to config Privoxy for Privoxy-Pilot
 
+
+gzip -k /usr/local/etc/privoxy/config.original
+cp /usr/local/etc/privoxy/config.original /usr/local/etc/privoxy/config
+rm /usr/local/etc/privoxy/config.bak
 echo -e "\r\n \r\n # do not edit above this line\r\n# add local configuration here\r\n# \r\n" >> /usr/local/etc/privoxy/config
 cat /usr/local/etc/privoxy/config.original >> /usr/local/etc/privoxy/config
 cp /usr/local/etc/privoxy/config /usr/local/etc/privoxy/config.bak
